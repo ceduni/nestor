ffe976c3a4a71652f19ea3c07740e4507fe3c158
rango <sidya.galakho@umontreal.ca>
2024-06-06 12:41:06 -0400

    correct typo

--- codebase/server/index.js
+++ codebase/server/index.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: './configs/.env' });
 const fastify = require('fastify')({ loggger: true });
 const mongoose = require('mongoose');
 const dev_port = Number(process.env.DEV_PORT);
-const mongodb_connection_string = process.env.MONGO_DB_CONNECTION_STRING;
+const mongodb_connection_string = process.env.MONGODB_CONNECTION_STRING;
 mongoose.connect(mongodb_connection_string, {
     useNewUrlParser: true,
     useUnifiedTopology: true
