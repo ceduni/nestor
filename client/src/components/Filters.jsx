1acef85e52604835b2a7316054b96d0295f6365e
MinPro <mbs0722@gmail.com>
2024-06-20 10:29:04 -0400

    Add Filter to Main

--- client/src/components/Filters.jsx
+++ client/src/components/Filters.jsx
@@ -8,29 +8,29 @@ import { FiFilter } from "react-icons/fi";
 
 export default function Filters() {
     return (
-        <div className=''>
-            <section className=''>
-                <h1 className='text-xl text-center'>Slogan</h1>
-                <form className='flex justify-center' action="">
-                    <div className='flex flex-col items-center'>
-                        <label htmlFor="">Nom de l'espace</label>
-                        <input type="text" placeholder="Entrer un nom"/>
+        <div className='filters p-2'>
+            <section className='filter_section flex flex-col items-center'>
+                <h1 className='text-xl p-2'>Slogan</h1>
+                <form className='filter_form flex justify-center rounded-full px-8 m-3' action="">
+                    <div className='filter_form_items flex flex-col gap-x-5 items-start'>
+                        <label className='filter_label' htmlFor="">Nom de l'espace</label>
+                        <input className='filter_input' type="text" placeholder="Entrer un nom"/>
                     </div>
-                    <div className='flex flex-col items-center'>
-                        <label htmlFor="">Adresse</label>
-                        <input type="text" placeholder="Entrer une adresse"/>
+                    <div className='filter_form_items flex flex-col items-start'>
+                        <label className='filter_label' htmlFor="">Adresse</label>
+                        <input className='filter_input' type="text" placeholder="Entrer une adresse"/>
                     </div>
-                    <div className='flex flex-col items-center'>
-                        <label htmlFor="">Date</label>
+                    <div className='filter_form_items flex flex-col items-start'>
+                        <label className='filter_label' htmlFor="">Date</label>
                         <input type="date" />
                     </div>
-                    <div>
-                        <a href="">
-                            <IoSearchCircle className='size-10'/>
+                    <div className='filter_form_btn flex justify-center items-center px-2'>
+                        <a className="filter_form_btn_link" href="">
+                            <IoSearchCircle className='size-12'/>
                         </a>
                     </div>
                 </form>
-                <div className='flex justify-center gap-10'>
+                <div className='flex justify-center gap-10 mx-2'>
                     <ul className='flex justify-center gap-8'>
                         <li>
                             <a className='filter_icons flex flex-col items-center' href="">
@@ -63,10 +63,12 @@ export default function Filters() {
                             </a>
                         </li>
                     </ul>
-                    <a className='flex gap-2 items-center' href="">
-                        <p>Filtres</p>
-                        <FiFilter />
-                    </a>
+                    <div className='filter_btn_container flex justify-center items-center'>
+                        <a className='filter_btn flex gap-2 items-center px-3 py-1 rounded-full' href="">
+                            <p>Filtres</p>
+                            <FiFilter />
+                        </a>
+                    </div>
                 </div>
             </section>
         </div>
