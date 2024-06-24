f6bc6e78152e68f5599b7b47d9efdc71417c933b
rango <sidya.galakho@umontreal.ca>
2024-06-24 03:59:52 -0400

    add availability routes

--- server/src/app.js
+++ server/src/app.js
@@ -2,15 +2,18 @@ require('dotenv').config();
 "use strict";
 const fastify = require('fastify')({ logger: true });
 const mongoose = require('mongoose');
-mongoose.set({ debug: true });
+//mongoose.set({ debug: true });
 // import routes
 const spaceRoutes = require("./routes/space.routes.js");
-
+const availabilityRoutes = require("./routes/availability.routes.js");
 // connect to database
 mongoose.connect(process.env.MONGODB_TEST_CONNECTION_STRING, {}).then(() => console.log("Connected to the database")).catch((e) => console.log("Error connecting to database", e));
 
 // start server
+
 fastify.register(spaceRoutes, { prefix: "/api/v1/spaces" });
+fastify.register(availabilityRoutes, { prefix: "/api/v1" });
+
 const start = () => {
   try {
     fastify.listen({ port: process.env.PORT }, (err) => {
