8bd90e2d5ee8442d721bc51879a1be3f5254515a
MinPro <mbs0722@gmail.com>
2024-06-24 18:37:40 -0400

    Update Cards style

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -54,6 +54,38 @@ export default function Cards() {
             peopleNum: "6",
             logoSrc: ""
         },
+        {
+            imgSrc : "https://images.unsplash.com/photo-1600431521340-491eca880813?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxpYnJhcnl8ZW58MHx8MHx8fDA%3D",
+            name: "Space name 7",
+            address: "Spaces address 7",
+            status: "Available",
+            peopleNum: "6",
+            logoSrc: ""
+        },
+        {
+            imgSrc : "https://images.unsplash.com/photo-1600431521340-491eca880813?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxpYnJhcnl8ZW58MHx8MHx8fDA%3D",
+            name: "Space name 8",
+            address: "Spaces address 8",
+            status: "Available",
+            peopleNum: "6",
+            logoSrc: ""
+        },
+        {
+            imgSrc : "https://images.unsplash.com/photo-1600431521340-491eca880813?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxpYnJhcnl8ZW58MHx8MHx8fDA%3D",
+            name: "Space name 9",
+            address: "Spaces address 9",
+            status: "Available",
+            peopleNum: "6",
+            logoSrc: ""
+        },
+        {
+            imgSrc : "https://images.unsplash.com/photo-1600431521340-491eca880813?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxpYnJhcnl8ZW58MHx8MHx8fDA%3D",
+            name: "Space name 10",
+            address: "Spaces address 10",
+            status: "Available",
+            peopleNum: "6",
+            logoSrc: ""
+        },
     ]);
     const [detailSelected, setDetailSelected] = useState({});
 
@@ -73,11 +105,11 @@ export default function Cards() {
     }
 
     return (
-        <section className='grid grid-cols-3 p-10'>
+        <section className='grid grid-cols-3 px-0'>
             <div className={cardSelected ?  
-                            'cards h-1/6 overflow-auto scrollbar-hidden grid grid-cols-1 gap-5 pl-20 pt-20' 
+                            'cards h-1/3 overflow-auto scrollbar-hidden grid grid-cols-1 gap-5 px-10 py-5' 
                             :
-                            'grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 px-20 py-5 col-span-3' }
+                            'cards h-1/2 overflow-auto scrollbar-hidden grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 px-10 py-5 col-span-3' }
             >   
                 {spaces.map((item) => (
                     <Card
@@ -87,7 +119,7 @@ export default function Cards() {
                     />
                 ))}
             </div>
-            <div className='card_detail h-1/6 overflow-auto flex justify-center items-start col-span-2 p-20'>
+            <div className='card_detail h-1/3 overflow-auto flex justify-center items-start col-span-2 px-10 py-5'>
                 {cardSelected ? <CardDetail spaceDetail={detailSelected}/> : <></>}
             </div>
         </section>
