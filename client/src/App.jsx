acb42c69ae6c63c71d42eef736663ff7085391d0
MinPro <mbs0722@gmail.com>
2024-06-27 17:36:01 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
@@ -8,12 +8,28 @@ function App() {
   // Modes pour nav items
   const [logedIn, setLogedin] = useState(false);
   const [isAdmin, setIsadmin] = useState(false);
+  const [data, setData] = useState(null);
+
+  useEffect(()=>{
+    fetch('/api/data')
+      .then(res => {
+        console.log("res : " + res);
+        res.json();
+      })
+      .then(data => {
+        console.log("data : " + data);
+        setData(data);
+      })
+      .catch(error => console.error(error));
+  }, []);
+  console.log(data)
 
   return (
     <div>
       <Header logedIn={logedIn} isAdmin={isAdmin}/>
       <Main />
       <Footer />
+      {data ? <p>{data.hello}</p> : <p>no msg</p>}
     </div>
   )
 }
