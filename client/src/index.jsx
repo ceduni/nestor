d2833054326c32a93389bebc478e10b5d62a56b2
MinPro <mbs0722@gmail.com>
2024-06-27 18:08:19 -0400

    Init react router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <App />
+}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
