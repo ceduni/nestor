6dd9bd206a90eee767a4d415385a904ef80837c9
MinPro <mbs0722@gmail.com>
2024-06-29 20:38:12 -0400

    Merged server folder from api-init

--- server/src/routes/space.routes.js
+++ server/src/routes/space.routes.js
@@ -6,10 +6,12 @@ async function getRoutes(fastify, options) {
   fastify.post("/", spaceService.addSpace);
   fastify.put("/:id", spaceService.updateSpace);
   fastify.delete("/:id", spaceService.removeSpace);
-  fastify.get("/:id/availabilities/", spaceService.getAvailabilities);
   fastify.post("/:id/availabilities/", spaceService.addAvailability);
   fastify.put("/:spaceId/availabilities/:availId", spaceService.updateAvailability);
   fastify.delete("/:spaceId/availabilities/:availId", spaceService.removeAvailability);
+  fastify.post("/:id/images/", spaceService.addImage);
+  fastify.put("/:spaceId/images/:imageId", spaceService.updateImage);
+  fastify.delete("/:spaceId/images/:imageId", spaceService.removeImage)
 }
 
 module.exports = getRoutes;
