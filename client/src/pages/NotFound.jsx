ec4f57cee26873477712ea706f225cd0cbbdfdcb
MinPro <mbs0722@gmail.com>
2024-07-07 15:00:55 -0400

    Update NotFound.jsx

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function NotFound() {
     return (
-        <div className='flex flex-col items-center gap-3'>
+        <div className='flex flex-col justify-center items-center gap-3'>
             <p className='text-5xl'>Oops!!</p>
             <p className='text-xl'>404 - PAGE NON TROUVÉE</p>
             <p>La page que vous recherchez a peut-être été supprimée, son nom a changé ou elle est temporairement indisponible.</p>
