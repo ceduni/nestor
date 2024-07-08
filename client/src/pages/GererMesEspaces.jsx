0d898d98e7020ca341de8af660261445504dce9a
MinPro <mbs0722@gmail.com>
2024-07-08 15:01:40 -0400

    update MesEspaces & GererMesEspaces

--- client/src/pages/GererMesEspaces.jsx
+++ client/src/pages/GererMesEspaces.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 export default function GererMesEspaces() {
     return (
-        <div>
-            Gerer Mes Espaces
-        </div>
+        <section>
+            <header className='pl-5 text-4xl bg-black h-40'>
+                <h1 className='text-white relative top-12 text-center'>Mes espaces</h1>
+            </header>
+            <article>
+                Mes cartes d'espaces
+            </article>
+        </section>
     );
 }
 
--- client/src/pages/MesReservations.jsx
+++ client/src/pages/MesReservations.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 export default function MesReservations() {
     return (
-        <div>
-            Mes Favoris
-        </div>
+        <section>
+            <header className='pl-5 text-4xl bg-black h-40'>
+                <h1 className='text-white relative top-12 text-center'>Mes Reservations</h1>
+            </header>
+            <article>
+                Mes cartes de réservations
+            </article>
+        </section>
     );
 }
