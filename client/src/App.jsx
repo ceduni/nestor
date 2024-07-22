f3447d65569684d36228aef693b37d131c882b86
MinPro <mbs0722@gmail.com>
2024-07-22 14:57:26 -0400

    Add ReservationsContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,20 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { LoginStatusProvider } from "./context/LoginStatusContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReservationsProvider } from './context/ReservationsContext';
 
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <LoginStatusProvider>
-      <QueryClientProvider client={queryClient}>
-        <Header />
-        <Outlet />
-        <Footer />
-      </QueryClientProvider>
+      <ReservationsProvider>
+        <QueryClientProvider client={queryClient}>
+          <Header />
+          <Outlet />
+          <Footer />
+        </QueryClientProvider>
+      </ReservationsProvider>
     </LoginStatusProvider>
   );
 }
