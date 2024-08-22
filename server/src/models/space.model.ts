5330b49a6860394ebc045e9318c742f2b4c931a3
rango <sidya.galakho@umontreal.ca>
2024-08-21 23:07:59 -0400

    implement space model

--- server/src/models/space.model.ts
+++ server/src/models/space.model.ts
@@ -1,1 +1,112 @@
 import mongoose from "mongoose";
+
+const imageSchema = new mongoose.Schema({
+    url: {
+        type: String,
+        required: true,
+    },
+    isMain: {
+        type: Boolean,
+        required: true,
+    },
+});
+const availabilitySchema = new mongoose.Schema({
+    startAt: {
+        type: Date,
+        required: true,
+    },
+    endAt: {
+        type: Date,
+        required: true,
+    },
+});
+
+const spaceSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        library: {
+            type: String,
+        },
+        street: {
+            type: String,
+            required: true,
+        },
+        city: {
+            type: String,
+            required: true,
+        },
+        state: {
+            type: String,
+            required: true,
+        },
+        postalCode: {
+            type: String,
+            required: true,
+        },
+        capacity: {
+            type: Number,
+            min: 1,
+            required: true,
+        },
+        isAvailable: {
+            type: Boolean,
+            default: true,
+        },
+        images: {
+            type: [imageSchema],
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        organisation: {
+            type: String,
+            required: true,
+        },
+        features: {
+            type: [String],
+            enum: [
+                "wifi",
+                "screen",
+                "plug",
+                "projector",
+                "noise cancelling",
+                "whiteboard",
+                "accessible",
+            ],
+            required: true,
+        },
+        availabilities: {
+            type: [availabilitySchema],
+            required: true,
+        },
+        type: {
+            type: [String],
+            enum: [
+                "university",
+                "library",
+                "facility",
+                "nature",
+                "coffee",
+                "laboratory",
+                "studyRoom",
+            ],
+            required: true,
+        },
+    },
+    { timestamps: true },
+);
+
+spaceSchema.methods.toJSON = function () {
+    const obj = this.toObject();
+    delete obj.__v;
+    return obj;
+};
+
+export const space = mongoose.model("space", spaceSchema);
+
