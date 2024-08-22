a50b1d5656c296ab25202b47242ab864d53a3a4f
rango <sidya.galakho@umontreal.ca>
2024-08-21 22:57:06 -0400

    init express server

---
+++ server/src/index.ts
@@ -0,0 +1,31 @@
+import "dotenv/config";
+import express, { Request, Response } from "express";
+const app = express();
+import mongoose, { Schema } from "mongoose";
+import { AddressInfo, Socket } from "net";
+const mongodb_connection_string =
+  process.env.MONGODB_DEV_CONNECTION_STRING ?? "";
+const port = Number(process.env.DEV_PORT) ?? 5000;
+
+app.get("/v1/spaces/locations", (req: Request, res: Response) => {
+  res.send(req.query);
+});
+
+mongoose
+  .connect(mongodb_connection_string)
+  .then((mongoose) => {
+    console.log("Connected successfully to MongoDB");
+
+    const server = app
+      .listen(port, "127.0.0.1", () => {
+        const { address, port } = server.address() as AddressInfo;
+        console.log("Server running at http://" + address + ":" + port);
+      })
+      .on("error", (error: Error) => {
+        console.error("Error connecting to server:", error);
+      });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
--- server/src/routes/location.routes.js
+++
@@ -1,7 +0,0 @@
-const locationService = require("../services/location.service");
-
-async function getRoutes(fastify) {
-  fastify.get("/", locationService.getCities);
-}
-
-module.exports = getRoutes;
