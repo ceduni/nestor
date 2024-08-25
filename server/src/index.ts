d942b8ec08f73f688a04c5fb4f2d5e2b6d630b2d
rango <sidya.galakho@umontreal.ca>
2024-08-25 15:55:49 +0000

    implement address filter

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,24 +2,49 @@ import "dotenv/config";
 import express, { Request, Response } from "express";
 const app = express();
 import mongoose from "mongoose";
+import cors from "cors";
 import { AddressInfo } from "net";
-const mongodb_connection_string =
-  process.env.MONGODB_DEV_CONNECTION_STRING ?? "";
+const mongodb_connection_string = process.env.MONGODB_DEV_CONNECTION_STRING ?? "";
 const port = Number(process.env.DEV_PORT) ?? 5000;
 
+app.use(cors());
 
 import {space} from "./models/space.model";
 app.get("/v1/spaces/locations", async (req: Request, res: Response) => {
     try {
         const page = Number(req.query.page);
         const limit = Number(req.query.limit);
-        const address = req.query.address;
+        const address = req.query.address as string;
+        const pattern = '^' + address.trim();
         const results = await space.aggregate([
             {
-                $limit: page*limit,
+                "$project": {
+                    "streetNumber": 1,
+                    "streetName": 1,
+                    "city": 1,
+                    "state": 1,
+                    "postalCode": 1,
+                    "country": 1,
+                    "_id": 0,
+                    "id": "$_id",
+                }},
+            {
+                "$match": {
+                    "$or": [
+                        { "streetNumber": { "$regex": pattern, "$options": "i"} },
+                        { "streetName": { "$regex": pattern, "$options": "i"} },
+                        { "city": { "$regex": pattern, "$options": "i" } },
+                        { "state": { "$regex": pattern, "$options": "i" } },
+                        { "postalCode": { "$regex": pattern, "$options": "i" } },
+                        { "country": { "$regex": pattern, "$options": "i" } }
+                    ]
+                }
             },
             {
-                $search: {address}
+                "$limit": limit
+            },
+            {
+                "$skip": page-1
             }
         ]);
         res.status(200).send(results);
