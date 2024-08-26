0cc5224d8aa9391168974bc5c2f3ae1e64d1ce90
rango <sidya.galakho@umontreal.ca>
2024-08-26 05:09:50 +0000

    implement address filter

--- client/src/features/filters/components/AddressDropDown.tsx
+++ client/src/features/filters/components/AddressDropDown.tsx
@@ -8,6 +8,24 @@ export default function AddressDropDown({locations}) {
         <div className="dropdown-container">
             {
                 locations.map(location => {
+                    const hasStreetInfo = location.streetNumber || location.streetName;
+                    // Determine the title and subtitle based on available information
+                    const title = hasStreetInfo
+                        ? `${location.streetNumber ? location.streetNumber + ' ' : ''}${location.streetName || ''}`
+                        : location.city
+                            ? location.city
+                            : location.state
+                                ? location.state
+                                : location.country; // Use country if only country is available
+
+                    const subtitle = !hasStreetInfo
+                        ? location.city
+                            ? `${location.state ? location.state + ', ' : ''}${location.country || ''}` // City with state and/or country
+                            : location.state
+                                ? `${location.country || ''}` // State without city; country as subtitle
+                                : '' // Empty subtitle if only country is used as title
+                        : ''; // Empty subtitle if street info is used as title
+
                     return(
                         <div className="dropdown-item-wrapper" key={location.id}>
                             <div className="dropdown-left-item">
@@ -15,11 +33,10 @@ export default function AddressDropDown({locations}) {
                                     <FaLocationDot />
                                 </div>
                                 <div className="dropdown-item-text-container">
-                                    <div className="dropdown-item-title" >{location.streetNumber} {location.streetName}</div>
-                                    <div className="dropdown-item-subtitle">{location.state}, {location.country}</div>
+                                    <div className="dropdown-item-title" >{title}</div>
+                                    <div className="dropdown-item-subtitle">{subtitle}</div>
                                 </div>
                             </div>
-                            <div className="distance-tag">2 km</div>
                         </div>
                     )
                 })
