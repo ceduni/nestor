52e69a5e8f8d423815df194a0daf6c4082aeddc8
rango <sidya.galakho@umontreal.ca>
2024-08-28 14:57:38 +0000

    implement space api

--- server/src/models/space.model.ts
+++ server/src/models/space.model.ts
@@ -1,120 +1,119 @@
 import mongoose from "mongoose";
 
 const imageSchema = new mongoose.Schema({
-    url: {
-        type: String,
-        required: true,
-    },
-    isMain: {
-        type: Boolean,
-        required: true,
-    },
+  url: {
+    type: String,
+    required: true,
+  },
+  isMain: {
+    type: Boolean,
+    required: true,
+  },
 });
 const availabilitySchema = new mongoose.Schema({
-    startAt: {
-        type: Date,
-        required: true,
-    },
-    endAt: {
-        type: Date,
-        required: true,
-    },
+  startAt: {
+    type: Date,
+    required: true,
+  },
+  endAt: {
+    type: Date,
+    required: true,
+  },
 });
 
 const spaceSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        library: {
-            type: String,
-        },
-        streetNumber: {
-            type: String,
-            required: true,
-        },
-        streetName: {
-            type: String,
-            required: true,
-        },
-        city: {
-            type: String,
-            required: true,
-        },
-        state: {
-            type: String,
-            required: true,
-        },
-        postalCode: {
-            type: String,
-            required: true,
-        },
-        country: {
-            type: String,
-            required: true
-        },
-        capacity: {
-            type: Number,
-            min: 1,
-            required: true,
-        },
-        isAvailable: {
-            type: Boolean,
-            default: true,
-        },
-        images: {
-            type: [imageSchema],
-            required: true,
-        },
-        description: {
-            type: String,
-            required: true,
-        },
-        organisation: {
-            type: String,
-            required: true,
-        },
-        features: {
-            type: [String],
-            enum: [
-                "wifi",
-                "screen",
-                "plug",
-                "projector",
-                "noise cancelling",
-                "whiteboard",
-                "accessible",
-            ],
-            required: true,
-        },
-        availabilities: {
-            type: [availabilitySchema],
-            required: true,
-        },
-        type: {
-            type: [String],
-            enum: [
-                "university",
-                "library",
-                "facility",
-                "nature",
-                "coffee",
-                "laboratory",
-                "studyRoom",
-            ],
-            required: true,
-        },
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    { timestamps: true },
+    library: {
+      type: String,
+    },
+    streetNumber: {
+      type: String,
+      required: true,
+    },
+    streetName: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    capacity: {
+      type: Number,
+      min: 1,
+      required: true,
+    },
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+    images: {
+      type: [imageSchema],
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    organisation: {
+      type: String,
+      required: true,
+    },
+    features: {
+      type: [String],
+      enum: [
+        "wifi",
+        "screen",
+        "plug",
+        "projector",
+        "noise cancelling",
+        "whiteboard",
+        "accessible",
+      ],
+      required: true,
+    },
+    availabilities: {
+      type: [availabilitySchema],
+      required: true,
+    },
+    type: {
+      type: [String],
+      enum: [
+        "university",
+        "library",
+        "facility",
+        "nature",
+        "coffee",
+        "laboratory",
+        "studyRoom",
+      ],
+      required: true,
+    },
+  },
+  { timestamps: true },
 );
 
 spaceSchema.methods.toJSON = function () {
-    const obj = this.toObject();
-    delete obj.__v;
-    return obj;
+  const obj = this.toObject();
+  delete obj.__v;
+  return obj;
 };
 
 export const space = mongoose.model("space", spaceSchema);
-
