b623118785c6e503b4b28fdefe4d0a53db6e144d
rango <sidya.galakho@umontreal.ca>
2024-10-10 15:38:30 -0400

    latest server version

--- server/src/routes/space.route.ts
+++ server/src/routes/space.route.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { query, Request, Response } from "express";
 import { space } from "../models/space.model";
 export const router = express.Router();
 
@@ -6,7 +6,19 @@ router.get("/spaces", async (req: Request, res: Response) => {
   try {
     const page = Number(req.query.page);
     const limit = Number(req.query.limit);
+    const address = req.query.address as string;
+    let addressParts;
+    if (address) {
+      addressParts = address.split(", ");
+    }
+
+    console.log(addressParts);
     const results = await space.aggregate([
+      {
+        $match: {
+          $and: [{}],
+        },
+      },
       {
         $limit: limit,
       },
@@ -25,8 +37,16 @@ router.get("/spaces/locations", async (req: Request, res: Response) => {
     const page = Number(req.query.page);
     const limit = Number(req.query.limit);
     const address = req.query.address as string;
-    const pattern = "^" + address.trim();
     const results = await space.aggregate([
+      {
+        $search: {
+          index: "default",
+          autocomplete: {
+            query: address.trim(),
+            path: "fullAddress",
+          },
+        },
+      },
       {
         $project: {
           streetNumber: 1,
@@ -37,18 +57,6 @@ router.get("/spaces/locations", async (req: Request, res: Response) => {
           country: 1,
           _id: 0,
           id: "$_id",
-        },
-      },
-      {
-        $match: {
-          $or: [
-            { streetNumber: { $regex: pattern, $options: "i" } },
-            { streetName: { $regex: pattern, $options: "i" } },
-            { city: { $regex: pattern, $options: "i" } },
-            { state: { $regex: pattern, $options: "i" } },
-            { postalCode: { $regex: pattern, $options: "i" } },
-            { country: { $regex: pattern, $options: "i" } },
-          ],
         },
       },
       {
