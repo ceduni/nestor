361111c92bdaa24d8ab7c624fee445b109243e49
rango <sidya.galakho@umontreal.ca>
2024-10-21 21:33:45 -0400

    implement default space query

--- server/src/routes/space.route.ts
+++ server/src/routes/space.route.ts
@@ -7,25 +7,38 @@ router.get("/spaces", async (req: Request, res: Response) => {
     const page = Number(req.query.page);
     const limit = Number(req.query.limit);
     const address = req.query.address as string;
-    let addressParts;
+    const capacity = Number(req.query.capacity) || 1;
+    const agg_pipeline = [];
     if (address) {
-      addressParts = address.split(", ");
-    }
-
-    console.log(addressParts);
-    const results = await space.aggregate([
-      {
-        $match: {
-          $and: [{}],
+      const search_pipeline = {
+        $search: {
+          index: "default",
+          autocomplete: {
+            query: address.trim(),
+            path: "fullAddress",
+          },
         },
+      };
+      agg_pipeline.push(search_pipeline);
+    }
+    agg_pipeline.push({
+      $match: {
+        $and: [
+          {
+            capacity: {
+              $gte: capacity,
+            },
+          },
+        ],
       },
-      {
-        $limit: limit,
-      },
-      {
-        $skip: page - 1,
-      },
-    ]);
+    });
+    agg_pipeline.push({
+      $limit: limit,
+    });
+    agg_pipeline.push({
+      $skip: page - 1,
+    });
+    const results = await space.aggregate(agg_pipeline);
     res.status(200).send(results);
   } catch (e) {
     res.status(500).send(e);
