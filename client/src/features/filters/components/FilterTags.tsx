d0458dea1fd164d58c3f609b3fe4c5e630479611
rango <sidya.galakho@umontreal.ca>
2024-10-31 19:06:25 -0400

    implement filter tags

--- client/src/features/filters/components/FilterTags.tsx
+++ client/src/features/filters/components/FilterTags.tsx
@@ -1,13 +1,38 @@
 import { GiCancel } from "react-icons/gi";
 import {useEffect, useState} from "react";
+import { format } from "date-fns";
 
 export default function FilterTags({queryParams}) {
+    const [tags, setTags] = useState([])
+    useEffect(() => {
+        setTags([])
+        if (queryParams.filters) {
+            const { date, address, capacity, features} = queryParams.filters
+            if(date) {
+                setTags(prevTag => [...prevTag, format(date, "yyyy-MM-dd")])
+            }
+            if(capacity) {
+                setTags(prevTag => [...prevTag, "capacité " +capacity])
+            }
+            if(address) {
+                setTags(prevTag => [...prevTag, address])
+            }
+            if(features.length !== 0) {
+                features.forEach(feature => setTags(prevTag => [...prevTag, feature]))
+            }
+        }
+    }, [queryParams]);
+
     return (
-        <div className="filter-tag-item">
-            <div className="filter-tag-item-container">
-                <div className="filter-tag-label">université</div>
-                <GiCancel className="cancel-icon"/>
+        <div className="filter-tag-container">
+            {tags.map((tag, index) => (
+            <div className="filter-tag-item" key={index}>
+                <div className="filter-tag-item-container">
+                    <div className="filter-tag-label">{tag}</div>
+                    <GiCancel className="cancel-icon"/>
+                </div>
             </div>
+            ))}
         </div>
     )
 }
