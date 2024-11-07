ab2e2054770e3f8b7663388db53d9ac764a61c4b
rango <sidya.galakho@umontreal.ca>
2024-11-06 19:57:00 -0500

    add loading spinner

--- client/src/features/cards/components/Cards.tsx
+++ client/src/features/cards/components/Cards.tsx
@@ -1,6 +1,7 @@
 import { FaSchool } from "react-icons/fa6";
 import { IoIosPeople } from "react-icons/io";
 import { motion } from "framer-motion";
+import {useEffect, useRef, useState} from "react";
 const container = {
     hidden: { opacity: 1, scale: 0 },
     visible: {
@@ -24,37 +25,91 @@ const item = {
     }
 }
 export default function Cards({ spaces }) {
+    const lastCardRef = useRef(null);
+    const [showLoader, setShowLoader] = useState(false)
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                const lastCard = entries[0];
+                if (lastCard.isIntersecting) {
+                    setShowLoader(true)
+                }
+            },
+            {
+                rootMargin: '0px',
+                threshold: 1.0,
+            }
+        );
+        if (lastCardRef.current) {
+            observer.observe(lastCardRef.current);
+        }
+
+        return () => {
+            if (lastCardRef.current) {
+                setShowLoader(false)
+                observer.unobserve(lastCardRef.current);
+            }
+        };
+    }, [spaces]);
   return (
     <section className="cards-section">
       <motion.div className="cards-container" variants={container} initial="hidden" animate="visible">
-          {spaces.map((space, index) => (
-              <motion.div key={index} className="card-container" variants={item}>
-                <div className="card-image">
-                  <img
-                    src={"src/assets/images/random-space.JPG"}
-                    alt={"space photo"}
-                  />
-                </div>
-                <div className="card-info-container">
-                  <div>
-                    <div className="card-info-name-container">
-                      <FaSchool className="school-logo " />
-                      <div className="card-info-name">{space.name}</div>
-                    </div>
-                    <div className="card-info-library">{space.library}</div>
-                    <div className="card-info-org">{space.organisation}</div>
+          {spaces.map((space, index) => {
+              if (index === spaces.length - 1) {
+                  return (
+                      <motion.div key={index} ref={lastCardRef} className="card-container" variants={item}>
+                          <div className="card-image">
+                              <img
+                                  src={"src/assets/images/random-space.JPG"}
+                                  alt={"space photo"}
+                              />
+                          </div>
+                          <div className="card-info-container">
+                              <div>
+                                  <div className="card-info-name-container">
+                                      <FaSchool className="school-logo "/>
+                                      <div className="card-info-name">{space.name}</div>
+                                  </div>
+                                  <div className="card-info-library">{space.library}</div>
+                                  <div className="card-info-org">{space.organisation}</div>
+                              </div>
+                              <div className="card-info-cap-container">
+                                  <IoIosPeople className="people-logo"/>
+                                  <div className="card-info-capacity">
+                                      Capacité {space.capacity}
+                                  </div>
+                              </div>
+                          </div>
+                      </motion.div>
+                  );
+              }
+              return <motion.div key={index} className="card-container" variants={item}>
+                  <div className="card-image">
+                      <img
+                          src={"src/assets/images/random-space.JPG"}
+                          alt={"space photo"}
+                      />
                   </div>
-                  <div className="card-info-cap-container">
-                    <IoIosPeople className="people-logo" />
-                    <div className="card-info-capacity">
-                      Capacité {space.capacity}
-                    </div>
+                  <div className="card-info-container">
+                      <div>
+                          <div className="card-info-name-container">
+                              <FaSchool className="school-logo "/>
+                              <div className="card-info-name">{space.name}</div>
+                          </div>
+                          <div className="card-info-library">{space.library}</div>
+                          <div className="card-info-org">{space.organisation}</div>
+                      </div>
+                      <div className="card-info-cap-container">
+                          <IoIosPeople className="people-logo"/>
+                          <div className="card-info-capacity">
+                              Capacité {space.capacity}
+                          </div>
+                      </div>
                   </div>
-                </div>
               </motion.div>
-          ))}
+          })}
       </motion.div>
-        <div className="loader"></div>
+        {showLoader && <div className="loader"></div>}
     </section>
   );
 }
